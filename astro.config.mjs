cb9019b697f1091c43866c1df722e6dff6991db4
matti <matti.new@gmail.com>
2025-11-24 00:47:44 +0100

    + Updated main app, that links to app 25.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/xmas-cards/25/SW', 
+  base: '/xmas-cards/',
   outDir: './dist'
 });
